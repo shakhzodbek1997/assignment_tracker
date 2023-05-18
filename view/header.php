2c835da505ef9525060f62218900b3f91f505a62
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-05-18 05:00:36 +0200

    styling with SASS

--- view/header.php
+++ view/header.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Assignment Tracker</title>
-  <link rel="stylesheet" href="view/css/main.min.css" />
+  <link rel="stylesheet" href="sass/main.css" />
 </head>
 
 <body>
